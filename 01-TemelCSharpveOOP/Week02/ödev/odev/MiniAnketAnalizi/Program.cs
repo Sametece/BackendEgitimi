04eb75238256aff6e4531d7d7b27e9afb9595779
Sametece <sametece1224@gmail.com>
2025-10-26 11:40:44 +0300

    .

--- 01-TemelCSharpveOOP/Week02/ödev/odev/MiniAnketAnalizi/Program.cs
+++ 01-TemelCSharpveOOP/Week02/ödev/odev/MiniAnketAnalizi/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] puanlar = new int[5];  // 5 Elemanlık bir Dizi oluşturuldu.
+
+        for (int i = 0; i < puanlar.Length; i++)
+        {
+            Console.Write($"Kişi {i + 1} için puan (1-10): ");
+            puanlar[i] = int.Parse(Console.ReadLine());
+        }
+
+        int toplam = 0;
+        int enYuksek = puanlar[0];
+        int enDusuk = puanlar[0];
+
+        for (int i = 0; i < puanlar.Length; i++)
+        {
+            toplam += puanlar[i];
+
+            if (puanlar[i] > enYuksek)
+                enYuksek = puanlar[i];
+
+            if (puanlar[i] < enDusuk)
+                enDusuk = puanlar[i];
+        }
+
+        double ortalama = (double)toplam / puanlar.Length;
+
+        Console.WriteLine($"\nOrtalama: {ortalama}");
+        Console.WriteLine($"En yüksek puan: {enYuksek}");
+        Console.WriteLine($"En düşük puan: {enDusuk}");
     }
-}
+
+  }
+    
+
---
+++ 01-TemelCSharpveOOP/Week03/ödevler/OdevCozum/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace OdevCozum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Liste Ortalaması Hesaplama
+
+            
+        #endregion
+
+        #region Sesli Harf Sayacı
+            
+        #endregion  
+
+        #region Döngü ile üs alma
+            
+        #endregion
+
+        #region Asal Sayı Avcısı
+            
+        #endregion
+
+        #region Palindromik Kelimeler
+            
+        #endregion
+
+        #region Mükemmel Sayı Bulucu
+            
+        #endregion
+
+        #region Listedeki En büyük ikinci Sayı
+            
+        #endregion
+
+        #region Metin Şifreleme (Sezar Şifrelemesi)
+            
+        #endregion
+
+        #region Basamakları Topla
+            
+        #endregion  
+
+        #region Fibonacci Dizisi oluşturma
+            
+        #endregion
+    }
+}
