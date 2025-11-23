2a4fdc84a0b0649f9b13340abf1ae61fadeb0c96
Sametece <sametece1224@gmail.com>
2025-11-23 11:21:59 +0300

    Update ResponseDto.cs

--- 05-WebApi/Week11/20-11-2025/ECommerce/ECommerce.Business/DTOs/ResponseDtos/ResponseDto.cs
+++ 05-WebApi/Week11/20-11-2025/ECommerce/ECommerce.Business/DTOs/ResponseDtos/ResponseDto.cs
@@ -1,28 +1,28 @@
 using System;
 
 namespace ECommerce.Business.DTOs.ResponseDtos;
-// Burada kullandığımız yapı Factory Design Pattern. Yani bu sınıf kendi tipinden nesneler yaratabilme yeteneğine sahip, bir fabrika gibi çalışıyor.
-public class ResponseDto<T>
+// Burada kullandığımız yapı Factory Design Pattern. 
+//Yani bu sınıf kendi tipinden nesneler yaratabilme yeteneğine sahip, bir fabrika gibi çalışıyor.
+public class ResponseDto<T> //Generic Yapı
 {
-    public T? Data { get; set; }
-    public string? Error { get; set; }
-    public bool IsSucceed { get; set; }
-    public int StatusCode { get; set; }
+    public T? Data { get; set; }  //Entityler buraya gelicek
+    public string? Error { get; set; } //Hata Mesajı
+    public bool IsSucceed { get; set; }  //Başarılı mı ?
+    public int StatusCode { get; set; } // Hata kodu -> Apıya Mesaj vericek
 
 
-
-    public static ResponseDto<T> Success(T? data, int statusCode)
+    public static ResponseDto<T> Success(T? data, int statusCode) //Kendi Tipinden Dön.
     {
         return new ResponseDto<T>
         {
             Data = data,
             StatusCode = statusCode,
-            IsSucceed = true
+            IsSucceed = true //Başarılı operasyonda true döner.
         };
     }
 
 
-    public static ResponseDto<T> Success(int statusCode)
+    public static ResponseDto<T> Success(int statusCode) //Delete-Update Gibi geriye veri dönmeden
     {
         return new ResponseDto<T>
         {
